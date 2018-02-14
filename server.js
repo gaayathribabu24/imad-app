6adec803934988b4a572ee20cc37ee333d9c87c9
gaayathribabu24 <36434792+gaayathribabu24@users.noreply.github.com>
2018-02-14 18:09:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
+var articles = {
+    'article-one': {
     title:'Article One | Gaayathri',
     heading: 'Article One',
     date:'Sep 5,2016',
@@ -22,6 +23,27 @@ var articleOne = {
                 This is the content for my first article.This is the content for my first article.This is the content for my first article.
                 This is the content for my first article.This is the content for my first article.This is the content for my first article.
             </p>`
+},
+    'article-two' : {
+        title:'Article Two | Gaayathri',
+    heading: 'Article Two',
+    date:'Sep 10,2016',
+    content:     `   <p>
+                This is the content for my second article.
+             
+            </p>
+              `
+    },
+    'article-three' : {
+         title:'Article Three | Gaayathri',
+    heading: 'Article Three',
+    date:'Sep 10,2016',
+    content:     `   <p>
+                This is the content for my third article.
+             
+            </p>
+              `
+    }
 };
 function createTemplate (data) {
     var title = data.title;
@@ -64,17 +86,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req,res) {
-res.send(createTemplate(articleOne));
+app.get('/:articleName',function (req,res) {
+    var articleName=req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });  
 
-app.get('/article-two',function (req,res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});  
-
-app.get('/article-three',function (req,res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});  
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
