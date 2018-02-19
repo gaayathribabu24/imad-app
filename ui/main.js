ae0344d7fe5fde05694299149ed7c91d3fae162f
gaayathribabu24 <36434792+gaayathribabu24@users.noreply.github.com>
2018-02-19 20:11:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,11 @@ button.onclick=function(){
 request.open('GET','http://gaayathribabu24.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+};
